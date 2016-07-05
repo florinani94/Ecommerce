60822f2fab3bfbaec1d4c528066b68644d55a12e
horatiucenean <horatiu.cenean@evozon.com>
2016-07-05 17:39:58 +0300

    initial commit of a hello world class

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by horatiucernean on 05/07/2016.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
