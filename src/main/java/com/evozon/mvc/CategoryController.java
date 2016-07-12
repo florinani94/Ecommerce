452ef072d51790c6eef84630882501a465306c52
mihai0matei <mihai.matei@evozon.com>
2016-07-12 17:24:04 +0300

    JAVAIS-26: CategoryController's path changed. Added "Controller".

--- src/main/java/com/evozon/mvc/CategoryController.java
+++ src/main/java/com/evozon/mvc/CategoryController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Created by mihai on 7/12/2016.
  */
 @Controller
+@RequestMapping(value = "Category")
 public class CategoryController {
 
     @Autowired
