0c81f5d5d3d7a613c1d20a94fbc1fea32088d2f8
mihai0matei <mihai.matei@evozon.com>
2016-07-13 12:01:12 +0300

    JAVAIS-29: getAllCategories() flow implemented.

--- src/main/java/com/evozon/dao/impl/CategoryDAOImpl.java
+++ src/main/java/com/evozon/dao/impl/CategoryDAOImpl.java
@@ -2,11 +2,15 @@ package com.evozon.dao.impl;
 
 import com.evozon.dao.CategoryDAO;
 import com.evozon.domain.Category;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by mihai on 7/12/2016.
@@ -22,4 +26,10 @@ public class CategoryDAOImpl implements CategoryDAO{
         Session session = sessionFactory.getCurrentSession();
         session.save(category);
     }
+
+    public List<Category> getAllCategories() {
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("FROM Category AS c").list();
+    }
+
 }
