a785123909f6a445d4686a937b85be4593c9f730
NicoaraDragos <dragos.nicoara@evozon.com>
2016-07-14 14:10:02 +0300

    JAVAIS: Style header

--- src/main/java/com/evozon/mvc/HomeController.java
+++ src/main/java/com/evozon/mvc/HomeController.java
@@ -27,7 +27,7 @@ class HomeController {
     /**
      * Simply selects the home view to render by returning its name.
      */
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String home(Locale locale, Model model) {
         logger.info("Welcome home! The client locale is {}.", locale);
 
@@ -38,7 +38,7 @@ class HomeController {
 
         model.addAttribute("serverTime", formattedDate);
 
-        return "home";
+        return "backofficeHeader";
     }
 
 
