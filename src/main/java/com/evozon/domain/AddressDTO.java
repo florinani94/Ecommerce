1fffae6145ec915bd90a766158671b807b2d9d0b
iuliacodau <iuliacodau@yahoo.com>
2016-07-21 14:14:16 +0300

    JAVAIS-39: Creation of AddressDTO class to hold data for both addresses

---
+++ src/main/java/com/evozon/domain/AddressDTO.java
@@ -0,0 +1,87 @@
+package com.evozon.domain;
+
+/**
+ * Created by iuliacodau on 21/07/2016.
+ */
+public class AddressDTO {
+
+    private String deliveryStreet;
+
+    private int deliveryNumber;
+
+    private String deliveryCity;
+
+    private String deliveryPhone;
+
+    private String billingStreet;
+
+    private int billingNumber;
+
+    private String billingCity;
+
+    private String billingPhone;
+
+    public String getDeliveryStreet() {
+        return deliveryStreet;
+    }
+
+    public void setDeliveryStreet(String deliveryStreet) {
+        this.deliveryStreet = deliveryStreet;
+    }
+
+    public int getDeliveryNumber() {
+        return deliveryNumber;
+    }
+
+    public void setDeliveryNumber(int deliveryNumber) {
+        this.deliveryNumber = deliveryNumber;
+    }
+
+    public String getDeliveryCity() {
+        return deliveryCity;
+    }
+
+    public void setDeliveryCity(String deliveryCity) {
+        this.deliveryCity = deliveryCity;
+    }
+
+    public String getDeliveryPhone() {
+        return deliveryPhone;
+    }
+
+    public void setDeliveryPhone(String deliveryPhone) {
+        this.deliveryPhone = deliveryPhone;
+    }
+
+    public String getBillingStreet() {
+        return billingStreet;
+    }
+
+    public void setBillingStreet(String billingStreet) {
+        this.billingStreet = billingStreet;
+    }
+
+    public int getBillingNumber() {
+        return billingNumber;
+    }
+
+    public void setBillingNumber(int billingNumber) {
+        this.billingNumber = billingNumber;
+    }
+
+    public String getBillingCity() {
+        return billingCity;
+    }
+
+    public void setBillingCity(String billingCity) {
+        this.billingCity = billingCity;
+    }
+
+    public String getBillingPhone() {
+        return billingPhone;
+    }
+
+    public void setBillingPhone(String billingPhone) {
+        this.billingPhone = billingPhone;
+    }
+}
