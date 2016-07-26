2135274f6052f03f315398ba7278ed9b1a1dec8d
iuliacodau <iuliacodau@yahoo.com>
2016-07-26 17:56:54 +0300

    JAVAIS 39: Created order key for mail

--- src/main/java/com/evozon/dao/OrderDAO.java
+++ src/main/java/com/evozon/dao/OrderDAO.java
@@ -5,21 +5,17 @@ import com.evozon.domain.Orders;
 
 import java.util.List;
 
-/**
- * TODO Remove "public" modifier from interfaces
- * It is permitted, but discouraged as a matter of style, to redundantly specify the public modifier
- * for a method declared in an interface.
- */
+
 public interface OrderDAO {
 
-    public Orders getOrderByKey(String orderKey);
+    Orders getOrderByKey(String orderKey);
 
-    public Orders getOrderById(Integer orderId);
+    Orders getOrderById(Integer orderId);
 
-    public void addOrder(Orders order);
+    void addOrder(Orders order);
 
-    public void updateOrder(Orders order);
+    void updateOrder(Orders order);
 
-    public List<Entry> getAllEntries(Integer orderId);
+    List<Entry> getAllEntries(Integer orderId);
 
 }
