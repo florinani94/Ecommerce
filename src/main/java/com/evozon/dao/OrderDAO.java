32bc2d80841e2ec9313df28cd8b7534e03fe43b9
andreidanciu <andrei.danciu@evozon.com>
2016-07-26 15:08:02 +0300

    TODOs after code review

--- src/main/java/com/evozon/dao/OrderDAO.java
+++ src/main/java/com/evozon/dao/OrderDAO.java
@@ -5,6 +5,11 @@ import com.evozon.domain.Orders;
 
 import java.util.List;
 
+/**
+ * TODO Remove "public" modifier from interfaces
+ * It is permitted, but discouraged as a matter of style, to redundantly specify the public modifier
+ * for a method declared in an interface.
+ */
 public interface OrderDAO {
 
     public Orders getOrderByKey(String orderKey);
